7161d66d75b0fec2ac2b46385a6646bd03da205f
ATM Simulation System <suyog1209shah@gmail.com>
2026-02-13 14:01:44 +0545

    pin

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 balance = 10000
 pin = "1234"
 entered_pin = input("Enter your PIN: ")
+if entered_pin == pin:
+    while True:
+        print("\n--- ATM MENU ---")
+        print("1. Check Balance")
+        print("2. Deposit Money")
+        print("3. Withdraw Money")
+        print("4. Exit")
